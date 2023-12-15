ae4dc0136c77477af131c18a12001aef7835e219
Ganesh <ganesh02003@gmail.com>
2023-12-15 20:58:01 +0530

    modified

--- Pattern5.java
+++ Pattern5.java
@@ -3,23 +3,17 @@
 *
 * *
 * * *
-* *
-*
+* * 
+* 
 
 */
 public class Pattern5 {
     public static void main(String[] args) {
-        int rows = 5;
+        int rows = 3;
     
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        for(int i=0;i<rows-1;i++){
-            for(int j=0;j<rows-i-1;j++){
+        for(int i=1;i<rows*2;i++){
+            int col = i <= rows ? i : 2*rows - i; 
+            for(int j=1;j<=col;j++){
                 System.out.print("* ");
             }
             System.out.println();
